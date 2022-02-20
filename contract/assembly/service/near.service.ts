4d1a2cbba928b990d0cc14ab2fbc1111625098f3
Malcolm Kiano <malcolmkiano@gmail.com>
2022-02-20 18:45:21 -0500

    refactor: remove uuid due to lack of AssemblyScript support

--- contract/assembly/service/near.service.ts
+++ contract/assembly/service/near.service.ts
@@ -1,37 +1,39 @@
 import { storage } from "near-sdk-as";
 import { AccountType, CreditAccount } from "../model";
-import { v4 as uuidv4 } from 'uuid';
 
 export default class NearService {
+
+
   /**
    * Creates a record indicating the opening of an account
+   * @param accountID the account identifier
    * @param uid the user's unique identifier
    * @param accountType the type of account
    * @param creditor the creditor's name/ID
    */
   openCreditAccount(
+    accountID: string,
     uid: string,
     accountType: AccountType,
     creditor: string
   ): void {
-    const recordID = uuidv4();
     const tempAccount: CreditAccount = {
       uid,
       accountType,
       creditor,
     };
-    storage.set<CreditAccount>(recordID, tempAccount);
+    storage.set<CreditAccount>(accountID, tempAccount);
   }
 
   /**
    * Finds and returns a requested credit account by ID
-   * @param recordID the record's unique id
+   * @param accountID the record's unique id
    * @returns requested credit account if found
    */
   getCreditAccount(
-    recordID: string
+    accountID: string
   ): CreditAccount | null {
-    return storage.get<CreditAccount>(recordID);
+    return storage.get<CreditAccount>(accountID);
   }
 }
 
