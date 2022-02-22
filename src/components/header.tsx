f9e5ee420925da20476748eac6276e3f8e04a4dc
Malcolm Kiano <malcolmkiano@gmail.com>
2022-02-22 18:47:50 -0500

    feat: add partial landing page & styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="relative z-50 flex items-center justify-between w-screen p-8">
-      <Link to="/" className="underline link-primary"><h1 className="text-xl font-extrabold text-white">BlockReport</h1></Link>
+    <header className="relative z-50 flex items-center justify-between w-screen wrapper">
+      <Link to="/" className="underline link-primary"><h1 className="text-2xl font-extrabold text-white">BlockReport</h1></Link>
     </header>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 
 import Login from "./views/login";
+import Landing from "./views/landing";
 
 const rootElement = document.querySelector("#root");
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="bureau" element={<>Bureau</>} />
-        <Route path="lender" element={<>Lender</>} />
-        <Route path="consumer" element={<>Consumer</>} />
-        <Route path="" element={<Login />} />
+        <Route requiresLogin={true} path="bureau" element={<>Bureau</>} />
+        <Route requiresLogin={true} path="lender" element={<>Lender</>} />
+        <Route requiresLogin={true} path="consumer" element={<>Consumer</>} />
+        <Route path="login" element={<Login />} />
+        <Route path="" element={<Landing />} />
       </Route>
     </Routes>
   </BrowserRouter>,
