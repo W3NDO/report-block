6112057287e71e7cc9f352bef3bf79d4f5c6d1b5
Malcolm Kiano <malcolmkiano@gmail.com>
2022-02-23 02:32:55 -0500

    refactor: adjust contract code for best practices and scalability

--- contract/assembly/model.ts
+++ contract/assembly/model.ts
@@ -1,25 +1,13 @@
 @nearBindgen
 export class CreditAccount {
   uid: string;
-  accountType: AccountType;
+  accountType: string;
   creditor: string;
 }
 
 @nearBindgen
-export class AccountType {
-  static Student: string = "0"
-  static Auto: string = "1"
-  static Home: string = "2"
-}
-
-@nearBindgen
 export class Response{
-  status: boolean;
-  data: ResponseData
-}
-
-@nearBindgen
-export class ResponseData{
-  TransactionId: string;
-  BlockHash: string;
+  success: boolean;
+  data: Map<string, string>;
+  message: string;
 }
