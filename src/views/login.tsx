aff8747df1f42f13ac1695a303e86344c2823810
Malcolm Kiano <malcolmkiano@gmail.com>
2022-02-22 20:49:54 -0500

    feat: add api service and context

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { ApiContext, UserType } from "../services/api";
 
 export default function Login() {
+  const api = React.useContext(ApiContext);
+  const navigate = useNavigate();
+  const login = (type: UserType) => api.login(type);
+
+  React.useEffect(() => {
+    if (api.isLoggedIn) {
+      navigate(`/${api.userType}`);
+    }
+  })
+
   return (
     <section className="absolute top-0 left-0 flex flex-col items-center justify-center w-screen h-screen">
       <article className="p-12 text-center rounded shadow-2xl bg-opacity-95 bg-neutral-900">
@@ -18,18 +29,16 @@ export default function Login() {
 
         <div className="flex flex-col">
           {[
-            ["Bureau", "business", "bureau"],
-            ["Lender", "account_balance", "lender"],
-            ["Consumer", "person", "consumer"],
-          ].map(([title, icon, path]) => (
-            <Link className="mb-3" tabIndex={-1} to={path}>
-              <button className="w-full btn-primary">
-                <span className="mr-3 material-icons">{icon}</span>
-                <span>
-                  Log in as <strong className="font-bold">{title}</strong>
-                </span>
-              </button>
-            </Link>
+            ["Bureau", "business", UserType.Bureau],
+            ["Lender", "account_balance", UserType.Lender],
+            ["Consumer", "person", UserType.Consumer],
+          ].map(([title, icon, userType]) => (
+            <button key={userType} onClick={() => login(userType as UserType)} className="w-full mb-3 btn-primary">
+              <span className="mr-3 material-icons">{icon}</span>
+              <span>
+                Log in as <strong className="font-bold">{title}</strong>
+              </span>
+            </button>
           ))}
         </div>
       </article>
